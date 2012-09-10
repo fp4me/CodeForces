6659fc080f4bf72095f64bdb0549b9fefb434aa2
f <f>
2012-09-10 16:44:52 +0200

    .

--- 220A - Little Elephant and Problem/Swap.hs
+++ 220A - Little Elephant and Problem/Swap.hs
@@ -32,55 +32,44 @@ getNumbers n = do
                  Just num -> (liftM (num:) (getNumbers(n-1)))
 
 
-data Push = Value Int Int Int | None | Already 
+data Push = Value Int Int Int | None | Already | Switch Int Int Int 
 data Last = One Int | Mul Int Int  
-
-swap:: Int -> [Int] -> Last -> Push  -> IO Bool 
 
 swap':: Int -> [Int] -> Last -> Push  -> IO Bool 
 swap' n [] _ _   | (n<=0) = return True 
 swap' n xs l p   | (n<=0) = swap n xs l p   
 swap' n t l p   = do
-                   nums <- getNumbers (2-(length t))
-                   swap (n-length(nums)) (t++nums) l p   
+                   nums <- getNumbers (3-(length t))
+                   swap (n-length(nums)) (t++nums) l p  
+
+swap:: Int -> [Int] -> Last -> Push  -> IO Bool 
+
+swap u (x:y:[]) (One l)  None | x>y = return $ y>= l
+swap u (x:y:[]) (Mul i l)  None | x>y = return $ y>= l
 
 swap u (x:y:xs) (One l)  p | (x==y) = swap' u (y:xs) (Mul x l)  p  
 swap u (x:y:xs) (Mul n l) p | (x==y) = case (x==n) of
                                       True -> swap' u (y:xs) (Mul n l) p 
                                       False -> swap' u (y:xs) (Mul x n) p 
 
-swap u (x:y:xs) p  Already | x>y =  return False
- 
-swap u (x:y:xs) last@(One l) None | x>y =  case (y>= l) of
-                                    True -> swap' u (y:xs) last $ Value l x y 
-                                    False -> return False
+swap u (x:y:xs) i  Already | x>y =  return False
 
+swap u (x:y:xs) i None | x>y =  case (y>=(getLast i)) of
+                               True -> case xs of
+                                  []  -> return True  
+                                  (z:zs) | z>=x -> swap' u (z:zs) (One x) $ Switch (getN i) y x
+                                  (z:zs) | otherwise -> case i of
+                                             Mul n _ ->  case (n==x) of
+                                                          True -> return False
+                                                          False -> swap' u (y:xs) i $ Value n x y 
+                                             One n -> swap' u (y:xs) i $ Value n x y 
+                               False -> return False
+  
+swap u (x:y:xs) i (Switch bInf a b) | x>y = return $  (b>=x)&&(y<=a)&&(y>=bInf)
 
-swap u (x:y:xs) last@(One l) (Value bInf v bSup) | x>y =  case  ((x<=v) && (bInf<=y) && (y<=bSup)) of
-                                                  True ->  swap' u (v:xs) (One x) Already  
-                                                  False ->  return False  
- 
-swap u (x:y:xs) (Mul n l) None | x>y = case (y >= n) of
-                                           True -> swap' u (y:xs) (Mul n l) $ Value n x y 
-                                           False -> case ((y>=l) && (y<x)) of
-                                            True -> swap' u (x:xs) (Mul n l) Already
-                                            False -> return False    
-
-swap u (x:y:xs) (Mul n l) (Value bInf v bSup ) | x>y =  case  ((x<=v) && (bInf<=y) && (y<=bSup)) of
-                                                  True ->  swap' u  (v:xs) (Mul n l) Already  
-                                                  False ->  return False
-
-swap u (x:y:xs) i@(One l) p@(Value bInf v bSup) = case (v<y) of
-                                        False -> swap' u (y:xs) (One x) p
-                                        True -> case (((x>=bInf) && ( x<=bSup)) || ((l==bSup) && (v==x))) of
-                                          True -> swap' u (v:y:xs) i Already
-                                          False -> return False
-
-swap u (x:y:xs) i@(Mul n l) p@(Value bInf v bSup) = case (v<y) of
-                                        False -> swap' u (y:xs) (One x) p
-                                        True -> case ( ((x>=bInf) && ( x<=bSup)) || ((l==bSup) && (v==n))) of
-                                          True -> swap' u (v:y:xs) i Already
-                                          False -> return False 
+swap u (x:y:xs) i p@(Value bInf v bSup) | x>y = case ((y>=bInf) && (y<=bSup) && (v>=(getN i))) of
+                                               False -> return False
+                                               True -> swap' u (v:xs) (One x) Already 
 
 swap u (x:y:xs) (Mul n l) p = case (n==x) of
                               True -> swap' u (y:xs) (Mul n l) p
@@ -88,15 +77,14 @@ swap u (x:y:xs) (Mul n l) p = case (n==x) of
 
 swap u (x:y:xs) i p = swap' u (y:xs) (One x) p
 
-swap u (x:[]) (Mul n l) (Value bInf v bSup) = return $ (l<=bSup) && (v>=n)
-swap u (x:[]) (One l) (Value bInf v bSup) = return $ ((x>=v) && (bSup==l)) || ((v>=l)&&(x>=bInf)&&(x<=bSup))
-swap u (x:[]) i p = case p of
-                   (Value bInf v bSup) -> return $  (v>=(getN i))&&(x>=bInf)&&(x<=bSup)
-                   otherwise -> return $ (x>= (getN i))  
+swap u (x:[]) i (Value bInf v bSup) = return $  ((bSup==(getN i))&&(v<=x)) || ((v>(getN i)) && (x>=bInf) && (x<= bSup))    
+swap u (x:[]) i p = return $ (x>= (getN i))  
 
 swap u ([]) i p = return True 
 
 getN (One n) = n
 getN (Mul n l) = n
- 
+
+getLast (One n) = n
+getLast (Mul n l) = l
 
